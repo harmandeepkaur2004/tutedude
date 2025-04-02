fd7b41afd5c93c13769359d8851fb1a8649c9269
Harmandeep <harmandeepk586@gmail.com>
2025-04-02 20:07:05 +0530

    3rd task

---
+++ Task3.py
@@ -0,0 +1,6 @@
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print(f"\n\033[95m{num}\033[0m \033[94mis\033[0m an even number.")
+else:
+    print(f"\n\033[95m{num}\033[0m \033[94mis\033[0m an odd number.")
