d86df1973e4038e994192b31d610399ceb62c565
Harmandeep <harmandeepk586@gmail.com>
2025-04-02 20:05:52 +0530

    2nd task

---
+++ Task2.py
@@ -0,0 +1,6 @@
+first_name = input("Enter your first \033[95mname\033[0m: ")
+last_name = input("Enter your last \033[91mname\033[0m: ")
+
+full_name = first_name + " " + last_name
+
+print(f"\nHello, {full_name}! Welcome \033[94mto\033[0m the Python program.")
